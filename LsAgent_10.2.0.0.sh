c12137ce3feb0c476ccc791b979688037e41899b
pio-007 <pio.maz@icloud.com>
2024-08-13 13:33:43 +0200

    Update LsAgent_10.2.0.0.sh

--- LsAgent_10.2.0.0.sh
+++ LsAgent_10.2.0.0.sh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
+#Introduction
+#This script is designed to check the status of the Lansweeper Agent (LsAgent) on a macOS system and perform an automatic update if necessary. It verifies the integrity of the LsAgent configuration file, checks for the correct version, and ensures that the agent is running and enabled. If any issues are detected, the script will download the latest version of the agent, mount the disk image, and install the updated version using the provided agent key. The script performs the following key functions:
+
+# 1. Checks the LsAgent configuration file: It verifies that the file exists, is not empty, and contains the correct version information, status, and agent key.
+# 2. Checks the installed version: If the configuration file is valid, it retrieves the currently installed version of the agent.
+# 3. Downloads and installs the latest version: If a newer version is available or the configuration file is invalid, it downloads the latest version of the agent from the provided URL, mounts the disk image, and installs the updated version using the agent key.
+# 4. Checks if the agent is running: Finally, it checks if the LsAgent service is currently running.
+
+
 SERVICE='LsAgent'
 LsAgent_URL="https://cdn.lansweeper.com/download/10.2.0/0/LsAgent-osx.dmg"
 LsAgent_ini="/Applications/LansweeperAgent/LsAgent.ini"
@@ -12,7 +21,6 @@ then
 	hdiutil detach $temp/mount
 	rm -rf $temp
 fi
-
 
 # Check that LanSweeper agent is working correctly
 if ! test -s $LsAgent_ini; then  x=1;echo "File not exist/is empty"; fi
